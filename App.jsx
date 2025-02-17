f4bb4f4011acdb9ff3b4faa45a94096d25b410e5
vaibhav <vaibhavzalte1004@gmail.com>
2025-02-17 14:57:27 +0530

    useState

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   Text,
@@ -8,26 +8,26 @@ import {
 } from 'react-native';
 import Login from './components/Login';
 
-const myFun = ()=>{
+const myFun = () => {
   console.warn('function called');
 };
 
-const printNumber = (num)=>{
-  console.warn('number is :',num);
+const printNumber = (num) => {
+  console.warn('number is :', num);
 };
 
 function App() {
-
+  const [count, setCount] = useState(0);
   return (
     <SafeAreaView>
       <View>
         <Text>hello sdf </Text>
       </View>
       {/* direct call */}
-      <Button onPress={myFun} title="Learn More" color={'green'}/>
+      <Button onPress={myFun} title="Learn More" color={'green'} />
       {/* pass parameter */}
-      <Button onPress={()=>printNumber(4)} title="Learn More" color={'red'}/>
-
+      <Button onPress={() => printNumber(4)} title="Learn More" color={'red'} />
+      <Button onPress={() => setCount(count + 1)} title={`${count}`} color={'black'} />
       <Login />
     </SafeAreaView>
   );

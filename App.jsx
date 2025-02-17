01300418a4eb826ac9258de0525ec22217bac24e
vaibhav <vaibhavzalte1004@gmail.com>
2025-02-17 14:46:37 +0530

    Button

--- App.jsx
+++ App.jsx
@@ -3,11 +3,18 @@ import {
   SafeAreaView,
   Text,
   View,
-  StyleSheet
+  Button,
+  Alert,
 } from 'react-native';
 import Login from './components/Login';
 
+const myFun = ()=>{
+  console.warn('function called');
+};
 
+const printNumber = (num)=>{
+  console.warn('number is :',num);
+};
 
 function App() {
 
@@ -16,11 +23,15 @@ function App() {
       <View>
         <Text>hello sdf </Text>
       </View>
+      {/* direct call */}
+      <Button onPress={myFun} title="Learn More" color={'green'}/>
+      {/* pass parameter */}
+      <Button onPress={()=>printNumber(4)} title="Learn More" color={'red'}/>
+
       <Login />
     </SafeAreaView>
   );
 }
-
 
 
 export default App;

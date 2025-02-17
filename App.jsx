d1ebceb5ba8f332073230d57fc7f28c458a8846e
vaibhav <vaibhavzalte1004@gmail.com>
2025-02-17 14:03:29 +0530

    initial setup

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+import Login from './components/Login';
+
+
+
+function App() {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>hello sdf </Text>
+      </View>
+      <Login />
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;

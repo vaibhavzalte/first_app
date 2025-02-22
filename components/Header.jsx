0fe9ea4426c7ea470950cf81ebabaad0cda93a6c
vaibhav <vaibhavzalte1004@gmail.com>
2025-02-22 18:30:01 +0530

    get data from redux store

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,16 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-export default function Header () {
-  const cardData = useSelector((state)=>state.reducer);
+export default function Header() {
+  const cardData = useSelector((state) => state.reducer);
+  const [cardItems, setCardItems] = useState(0);
+  useEffect(() => {
+    setCardItems(cardData.length);
+  }, [cardData]);
   return (
     <View>
-      <Text style={{fontSize:30,textAlign:'right',marginRight:20}}>{cardData.length}</Text>
+      <Text style={{ fontSize: 30, textAlign: 'right', marginRight: 20 }}>{cardItems}</Text>
     </View>
   )
 }

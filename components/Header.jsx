d751e86f9da1dbedd3b1fffa6ba2749d73e4079d
vaibhav <vaibhavzalte1004@gmail.com>
2025-02-22 18:27:58 +0530

    get data from redux store simply

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-export default function () {
+export default function Header () {
+  const cardData = useSelector((state)=>state.reducer);
   return (
     <View>
-      <Text style={{fontSize:30,textAlign:'right',marginRight:20}}>0</Text>
+      <Text style={{fontSize:30,textAlign:'right',marginRight:20}}>{cardData.length}</Text>
     </View>
   )
 }
